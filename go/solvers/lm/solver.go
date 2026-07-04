9bbde18763a02da606c1f8cd56380795ba5d5451
Red Daly <reddaly@google.com>
2026-07-04 03:30:35 +0000

    docs: extract package docstrings into doc.go files

--- go/solvers/lm/solver.go
+++ go/solvers/lm/solver.go
@@ -1,7 +1,3 @@
-// Package lm implements a 2D geometric constraint solver using the
-// Levenberg-Marquardt (LM) optimization algorithm.
-//
-// It is optimized for high performance and zero heap allocations in the hot loop.
 package lm
 
 import (
